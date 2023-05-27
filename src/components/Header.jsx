933fb4aefb8bd4b02448d3ae79d42c60e6ff846f
shrutivtu <shrutisaxena1596@gmail.com>
2023-05-27 16:09:49 +0530

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import { Heading, Image, Text } from "@chakra-ui/react";
+import logo from '../assets/light-bulb.svg';
+
+export const Header = () => {
+    return(
+    <>
+        <Image src={logo} alt='logo' width={100} marginBottom='1rem' />
+        <Heading color='white' marginBottom='1rem'>
+            AI Keyword Extractor
+        </Heading>
+        <Text fontSize={25} textAlign='center'>
+            Paste in your text below and we will extract the keywords for you
+        </Text>
+    </>
+    )
+}
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export * from './Header';
+export * from './Footer';
+export * from './TextInput';
+export * from './KeywordsModal';
