c500172056fc78acf6f12d680df0c318c9acd007
shrutivtu <shrutisaxena1596@gmail.com>
2023-06-20 13:53:35 +0530

    feat: implementation of hook for keyword extraction

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,10 @@
-import { useState } from "react";
 import { Container, Box } from "@chakra-ui/react";
 import { Header, Footer, TextInput, KeywordsModal } from "./components";
+import useKeywordExtraction from "./hooks/useKeywordExtraction";
 
 const App = () => {
-  const [keywords, setKeywords] = useState([]);
-  const [isOpen, setIsOpen] = useState(false);
-  const [loading, setLoading] = useState(false);
 
-  const extractKeywords = async (text) => {
-    setLoading(true);
-    setIsOpen(true);
-
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
-      },
-      body: JSON.stringify({
-        model: "text-davinci-003",
-        prompt:
-          "Extract the keywords from this text: " + text + "for the following information- Number of bedrooms, Number of bathrooms , Arrival date, Duration of stay , Number of adults , Number of children , Ages of children, Desired rating of accommodation, Preference for showing full price, Maximum price per night , Preferences for amenities - options: pool, TV, internet, desk, microwave, refrigerator, garden. Return an object with key value pair. Enclose all the keys in double quotes as well." +
-          "",
-        temperature: 0.13,
-        max_tokens: 180,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-      }),
-    };
-
-    try {
-      const response = await fetch(
-        import.meta.env.VITE_OPENAI_API_URL,
-        options
-      );
-      const json = await response.json();
-      setKeywords(json.choices[0].text.trim());
-      setLoading(false);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const closeModal = () => {
-    setIsOpen(false);
-  };
+  const { fetchData, keywords, loading, isOpen, closeModal } = useKeywordExtraction();
 
   return (
     <Box 
@@ -60,7 +19,7 @@ const App = () => {
       >
         <Header />
         <TextInput 
-          extractKeywords={extractKeywords} 
+          extractKeywords={fetchData} 
         />
         <Footer />
       </Container>
---
+++ src/hooks/useKeywordExtraction.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+const useKeywordExtraction = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [keywords, setKeywords] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async (text) => {
+    setLoading(true);
+    setIsOpen(true);
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "text-davinci-003",
+        prompt:
+          "Extract the keywords from this text: " +
+          text +
+          "",
+        temperature: 0.13,
+        max_tokens: 180,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      }),
+    };
+
+    try {
+      const response = await fetch(
+        import.meta.env.VITE_OPENAI_API_URL,
+        options
+      );
+      const json = await response.json();
+      setKeywords(json.choices[0].text.trim());
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  return { fetchData, keywords, loading, isOpen, closeModal };
+};
+
+export default useKeywordExtraction;
